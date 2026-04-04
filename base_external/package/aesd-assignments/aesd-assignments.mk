2f317e3c040b04d75fb300f4deddedaf4b3922cf
vd10 <vd10@ubuntu2204.myguest.virtualbox.org>
2026-04-05 00:57:38 +0300

    update commit hash for assignment3 repo.

--- base_external/package/aesd-assignments/aesd-assignments.mk
+++ base_external/package/aesd-assignments/aesd-assignments.mk
@@ -6,7 +6,7 @@
 ##############################################################
 
 #TODO: Fill up the contents below in order to reference your assignment 3 git contents
-AESD_ASSIGNMENTS_VERSION = 'a68dd191553ab2cd030db346685f573a88208bb2'
+AESD_ASSIGNMENTS_VERSION = '8869ae7340dbf38d71731bba35e5d0eb4746f031'
 # Note: Be sure to reference the *ssh* repository URL here (not https) to work properly
 # with ssh keys and the automated build/test system.
 # Your site should start with git@github.com:
